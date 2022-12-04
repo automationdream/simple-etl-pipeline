f875a91b8b902a57bc8d7bf678a2193c1959af4d
automationdream <automationdream@gmail.com>
2022-12-04 21:37:58 +0100

    implement find_dataset method

--- ETL.py
+++ ETL.py
@@ -1,3 +1,5 @@
+from typing import Union
+from models.commons import DataType
 from settings import Settings
 import pandas as pd
 from sqlalchemy import create_engine
@@ -10,12 +12,14 @@ class SpotifyDailyTop50Extraction:
         self.playlist_metadata = pd.DataFrame()
         self.data_folder = settings.data_folder
         self.database_url = settings.database_url
+        self.playlist_metadata_pattern = settings.playlist_metadata_pattern
+        self.playlist_tracks_metadata_pattern = (
+            settings.playlist_tracks_metadata_pattern
+        )
+        self.tracks_metadata_pattern = settings.tracks_metadata_pattern
 
     def get_playlist_metadata(self) -> pd.DataFrame:
-        # TODO: FIND playlist_metadata
-        playlist_metadata: Path = Path(self.data_folder).joinpath(
-            "112022_df_playlist_metadata.csv"
-        )
+        playlist_metadata: Path = self.find_dataset(DataType.PlaylistMetadata)
         self.playlist_metadata = pd.read_csv(playlist_metadata, index_col="Unnamed: 0")
         return self.playlist_metadata
 
@@ -38,3 +42,25 @@ class SpotifyDailyTop50Extraction:
                 index=False,
             )
             return True
+        return False
+
+    def find_dataset(self, dataset_type: DataType) -> Union[Path, None]:
+        """Find first dataset matching the pattern"""
+        directory = Path(self.data_folder)
+        pattern = None
+
+        if dataset_type == DataType.PlaylistMetadata:
+            pattern = self.playlist_metadata_pattern
+        elif dataset_type == DataType.PlaylistTracksMetadata:
+            pattern = self.playlist_tracks_metadata_pattern
+        elif dataset_type == DataType.TracksMetadata:
+            pattern = self.tracks_metadata_pattern
+        else:
+            NotImplementedError()
+
+        lookup = directory.rglob(f"*{pattern}")
+        results = list(lookup)
+        if results:
+            return results[0]
+
+        return None
